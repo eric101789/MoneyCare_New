0a88b7789472e1fb369ef2c6fc75de3c6d11de2d
EricYANG <eric10179@gmail.com>
2023-03-19 22:54:49 +0800

    2023/03/19 Add Life-Cycle-1

--- app/src/main/java/com/example/moneycare_new/MainActivity.java
+++ app/src/main/java/com/example/moneycare_new/MainActivity.java
@@ -3,12 +3,76 @@ package com.example.moneycare_new;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView theDate, theTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bookkeeping_constraintlayout);
+
+        uiInit();
+    }
+
+    private void uiInit() {
+        theDate = findViewById(R.id.textView5);
+        theTime = findViewById(R.id.textView6);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        varInit();
+        setListener();
+    }
+
+    private void varInit() {
+    }
+
+    private void setListener() {
+        theDate.setOnClickListener(this);
+        theTime.setOnClickListener(this);
+    }
+
+
+    @Override
+    protected void onStop() {
+        releaseUiListener();
+        super.onStop();
+    }
+
+    private void releaseUiListener() {
+        theDate.setOnClickListener(null);
+        theTime.setOnClickListener(null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
