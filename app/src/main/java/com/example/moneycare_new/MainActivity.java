1c3932077b2cda08e2864f293c143b5bfc6b98f4
EricYANG <eric10179@gmail.com>
2023-03-19 23:26:58 +0800

    2023/03/19 自動切換啟動畫面

--- app/src/main/java/com/example/moneycare_new/MainActivity.java
+++ app/src/main/java/com/example/moneycare_new/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -22,6 +25,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         count++; //++:遞增運算子；等於count = count +1;
 
         Log.d(TAG, "enter onCreate(), #" + count);
+
+        // 使用Handler自動切換螢幕畫面
+        Message msg = myHandler.obtainMessage();    // 從Message pool裡面取一個message出來
+        // 比新建立一個有效率
+        // Ctrl+Q 查看指令文件
+        myHandler.sendMessageDelayed(msg, 500); // 0.5秒後執行
     }
 
 
@@ -82,4 +91,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    // 宣告一個Handler物件，並且連結到UI Thread
+    private Handler myHandler = new Handler(Looper.getMainLooper()){
+
+        @Override
+        public void handleMessage(Message msg) {
+            // 這裡不處理msg內容，所以收到任何msg都是執行Activity切換的工作
+            startActivity(new Intent(MainActivity.this, Bookkeeping.class));
+
+            // 增加過場動畫
+            overridePendingTransition(android.R.anim.slide_in_left,
+                    android.R.anim.slide_out_right);
+            MainActivity.this.finish();
+        }
+    };
 }
